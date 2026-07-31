b70639fe5e73395b814a90868866770f9976b484
cybalicistjt-stack <cybalicist.jt@gmail.com>
2026-07-31 18:52:45 -0500

    Cache Content Forge and Pack List modules offline

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const CACHE = 'multiversal-aioc-v3';
+const CACHE = 'multiversal-aioc-v4';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
+  './authoring.css',
   './multiversal-seed.js',
   './app.js',
+  './authoring.js',
   './manifest.webmanifest',
   './icon-192.svg',
   './icon-512.svg'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
@@ -28,7 +28,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
   event.respondWith(
     caches.match(event.request).then(cached =>
       cached || fetch(event.request).catch(() => caches.match('./index.html'))
